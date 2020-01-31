453b3a6748ee532202743a440b32e6f7ffb3607f
Dedicatus <allenzym@gmail.com>
2020-01-30 17:39:01 -0800

    quick start under construction

---
+++ Assets/Scripts/GameSetupController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class GameSetupController : MonoBehaviour
+{
+
+	public Gameobject playerPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreatePlayer(); //Create a networked player object for ach player that loads in to the multiplayer room;
+    }
+
+    private void CreatePlayer()
+    {
+    	Debug.Log("Creating Player");
+    	PhotonNetwork.Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+    }
+
+}
